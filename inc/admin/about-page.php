1a71c82abc42f2491b8c52d4d1e25bec227fc665
George <george.olaru@pixelgrade.com>
2017-05-11 16:18:51 +0300

    Added content #42

--- inc/admin/about-page.php
+++ inc/admin/about-page.php
@@ -22,7 +22,7 @@ function noahlite_admin_setup() {
 		// Main welcome title
 		'welcome_title'         => sprintf( __( 'Welcome to %s! - Version ', 'noah-lite' ), 'Noah Lite' ),
 		// Main welcome content
-		'welcome_content'       => esc_html__( 'Noah Lite is a free one page WordPress theme. It\'s perfect for web agency business,corporate business,personal and parallax business portfolio, photography sites and freelancer.Is built on BootStrap with parallax support, is responsive, clean, modern, flat and minimal. Noah Lite is ecommerce (WooCommerce) Compatible, WPML, RTL, Retina-Ready, SEO Friendly and with parallax, full screen image is one of the best business themes.', 'noah-lite' ),
+		'welcome_content'       => esc_html__( ' Noah Lite is a free photography WordPress theme that creates momentum and makes a lovely intro to your journey.', 'noah-lite' ),
 		/**
 		 * Tabs array.
 		 *
@@ -42,9 +42,9 @@ function noahlite_admin_setup() {
 			'first' => array (
 				'title' => esc_html__( 'Contact Support','noah-lite' ),
 				'icon' => 'dashicons dashicons-sos',
-				'text' => esc_html__( 'We want to make sure you have the best experience using Noah Lite and that is why we gathered here all the necessary informations for you. We hope you will enjoy using Noah Lite, as much as we enjoy creating great products.','noah-lite' ),
+				'text' => __( 'We want to make sure you have the best experience using Noah Lite. If you <strong>do not have a paid upgrade</strong>, please post your question in our community forums.','noah-lite' ),
 				'button_label' => esc_html__( 'Contact Support','noah-lite' ),
-				'button_link' => esc_url( 'https://themeisle.com/contact/' ),
+				'button_link' => esc_url( 'https://wordpress.org/support/theme/noah-lite' ),
 				'is_button' => true,
 				'is_new_tab' => true
 			),
@@ -52,51 +52,24 @@ function noahlite_admin_setup() {
 				'title' => esc_html__( 'Documentation','noah-lite' ),
 				'icon' => 'dashicons dashicons-book-alt',
 				'text' => esc_html__( 'Need more details? Please check our full documentation for detailed information on how to use Noah Lite.','noah-lite' ),
-				'button_label' => esc_html__( 'Read full documentation','noah-lite' ),
-				'button_link' => 'https://docs.themeisle.com/article/5-zerif-lite-documentation',
-				'is_button' => false,
-				'is_new_tab' => true
-			),
-			'third' => array(
-				'title' => esc_html__( 'Changelog','noah-lite' ),
-				'icon' => 'dashicons dashicons-portfolio',
-				'text' => esc_html__( 'Want to get the gist on the latest theme changes? Just consult our changelog below to get a taste of the recent fixes and features implemented.','noah-lite' ),
-				'button_label' => esc_html__( 'Changelog','noah-lite' ),
-				'button_link' => esc_url( admin_url( 'themes.php?page=noahlite-welcome&tab=changelog&show=yes' ) ),
-				'is_button' => false,
-				'is_new_tab' => false
-			),
-			'fourth' => array(
-				'title' => esc_html__( 'Create a child theme','noah-lite' ),
-				'icon' => 'dashicons dashicons-admin-customizer',
-				'text' => esc_html__( "If you want to make changes to the theme's files, those changes are likely to be overwritten when you next update the theme. In order to prevent that from happening, you need to create a child theme. For this, please follow the documentation below.",'noah-lite' ),
-				'button_label' => esc_html__( 'View how to do this','noah-lite' ),
-				'button_link' => 'https://docs.themeisle.com/article/14-how-to-create-a-child-theme',
-				'is_button' => false,
-				'is_new_tab' => true
-			),
-			'fifth' => array(
-				'title' => esc_html__( 'Speed up your site','noah-lite' ),
-				'icon' => 'dashicons dashicons-controls-skipforward',
-				'text' => esc_html__( 'If you find yourself in the situation where everything on your site is running very slow, you might consider having a look at the below documentation where you will find the most common issues causing this and possible solutions for each of the issues.','noah-lite' ),
-				'button_label' => esc_html__( 'View how to do this','noah-lite' ),
-				'button_link' => 'https://docs.themeisle.com/article/63-speed-up-your-wordpress-site',
-				'is_button' => false,
-				'is_new_tab' => true
-			),
-			'sixth' => array(
-				'title' => esc_html__( 'Build a landing page with a drag-and-drop content builder','noah-lite' ),
-				'icon' => 'dashicons dashicons-images-alt2',
-				'text' => esc_html__( 'In the below documentation you will find an easy way to build a great looking landing page using a drag-and-drop content builder plugin.','noah-lite' ),
-				'button_label' => esc_html__( 'View how to do this','noah-lite' ),
-				'button_link' => 'https://docs.themeisle.com/article/219-how-to-build-a-landing-page-with-a-drag-and-drop-content-builder',
+				'button_label' => esc_html__( 'Read The Documentation','noah-lite' ),
+				'button_link' => 'https://pixelgrade.com/noah-lite-documentation/',
 				'is_button' => false,
 				'is_new_tab' => true
 			)
 		),
 		// Getting started tab
 		'getting_started' => array(
-			'first' => array (
+			'first' => array(
+				'title' => esc_html__( 'Go to Customizer','noah-lite' ),
+				'text' => esc_html__( 'Using the WordPress Customizer you can easily customize every aspect of the theme.','noah-lite' ),
+				'button_label' => esc_html__( 'Go to Customizer','noah-lite' ),
+				'button_link' => esc_url( admin_url( 'customize.php' ) ),
+				'is_button' => true,
+				'recommended_actions' => false,
+				'is_new_tab' => true
+			),
+			'second' => array (
 				'title' => esc_html__( 'Recommended actions','noah-lite' ),
 				'text' => esc_html__( 'We have compiled a list of steps for you, to take make sure the experience you will have using one of our products is very easy to follow.','noah-lite' ),
 				'button_label' => esc_html__( 'Recommended actions','noah-lite' ),
@@ -105,21 +78,12 @@ function noahlite_admin_setup() {
 				'recommended_actions' => true,
 				'is_new_tab' => false
 			),
-			'second' => array(
-				'title' => esc_html__( 'Read full documentation','noah-lite' ),
+			'third' => array(
+				'title' => esc_html__( 'Read the documentation','noah-lite' ),
 				'text' => esc_html__( 'Need more details? Please check our full documentation for detailed information on how to use Noah Lite.','noah-lite' ),
 				'button_label' => esc_html__( 'Documentation','noah-lite' ),
-				'button_link' => 'http://docs.themeisle.com/article/5-zerif-lite-documentation',
+				'button_link' => 'https://pixelgrade.com/noah-lite-documentation/',
 				'is_button' => false,
-				'recommended_actions' => false,
-				'is_new_tab' => true
-			),
-			'third' => array(
-				'title' => esc_html__( 'Go to Customizer','noah-lite' ),
-				'text' => esc_html__( 'Using the WordPress Customizer you can easily customize every aspect of the theme.','noah-lite' ),
-				'button_label' => esc_html__( 'Go to Customizer','noah-lite' ),
-				'button_link' => esc_url( admin_url( 'customize.php' ) ),
-				'is_button' => true,
 				'recommended_actions' => false,
 				'is_new_tab' => true
 			)
@@ -128,90 +92,90 @@ function noahlite_admin_setup() {
 		'free_pro'                => array(
 			'free_theme_name'     => 'Noah Lite',
 			'pro_theme_name'      => 'Noah PRO',
-			'pro_theme_link'      => 'https://pixelgrade.com/themes/noah/',
-			'get_pro_theme_label' => sprintf( __( 'Get %s now!', 'noah-lite' ), 'Noah Pro' ),
+			'pro_theme_link'      => 'https://pixelgrade.com/themes/noah-lite/?utm_source=noah-lite-clients&utm_medium=about-page&utm_campaign=noah-lite#pro',
+			'get_pro_theme_label' => sprintf( __( 'View %s', 'noah-lite' ), 'Noah Pro' ),
 			'features'            => array(
 				array(
-					'title'       => __( 'Parallax effect', 'noah-lite' ),
-					'description' => __( 'Smooth, catchy and easy scrolling experience.', 'noah-lite' ),
+					'title'       => __( 'Charming Design', 'noah-lite' ),
+					'description' => __( 'The visual approach highlights your unique personality and sustains a way of building your true-to-yourself brand.', 'noah-lite' ),
 					'is_in_lite'  => 'true',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Mobile friendly', 'noah-lite' ),
-					'description' => __( 'Responsive layout. Works on every device.', 'noah-lite' ),
+					'title'       => __( 'Mobile-Ready and Responsive for All Devices', 'noah-lite' ),
+					'description' => __( 'We knows how much you love your content and how critical it is to showcase it beautifully to all of your users, no matter the device they happen to use.', 'noah-lite' ),
 					'is_in_lite'  => 'true',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'WooCommerce Compatible', 'noah-lite' ),
-					'description' => __( 'Ready for e-commerce. You can build an online store here.', 'noah-lite' ),
+					'title'       => __( 'Steady SEO', 'noah-lite' ),
+					'description' => __( 'We\'ve made everything it requires in terms of SEO practices so that you can have a proper start. In the end, everyone has a thing for how they show up in search engines.', 'noah-lite' ),
 					'is_in_lite'  => 'true',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Frontpage sections', 'noah-lite' ),
-					'description' => __( 'Big title, Our focus, About us, Our team, Testimonials, Ribbons, Latest news, Contat us', 'noah-lite' ),
+					'title'       => __( 'Developer Friendly', 'noah-lite' ),
+					'description' => __( 'Writing clean, structured, consistent, smart code means that it can be easily extended by you or any developer you hire. With Noah, nothing but the latest WordPress features and standards are used.', 'noah-lite' ),
 					'is_in_lite'  => 'true',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Background image', 'noah-lite' ),
-					'description' => __( 'You can use any background image you want.', 'noah-lite' ),
-					'is_in_lite'  => 'true',
-					'is_in_pro'   => 'true',
-				),
-				array(
-					'title'       => __( 'Unlimited color option', 'noah-lite' ),
-					'description' => __( 'You can change the colors of each section. You have unlimited options.', 'noah-lite' ),
+					'title'       => __( 'Galleries Slideshows', 'noah-lite' ),
+					'description' => __( 'Beautifull zoom-in transitions from grid based gallery to slideshow.', 'noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Google map section', 'noah-lite' ),
-					'description' => __( 'Embed your current location to your website by using a Google map.','noah-lite' ),
+					'title'       => __( 'Thrilling Page Transitions', 'noah-lite' ),
+					'description' => __( 'The whole experience is packed into a fascinating journey that engages in an experiment that goes beyond what’s ordinary and familiar.', 'noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Portfolio', 'noah-lite' ),
-					'description' => __( 'Showcase your best projects in the portfolio section.', 'noah-lite' ),
+					'title'       => __( 'Home Page Slideshow', 'noah-lite' ),
+					'description' => __( 'Bring your best photos or videos in the front of the world by adding them into a front page slideshow.','noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Sections order', 'noah-lite' ),
-					'description' => __( 'Arrange the sections by your priorities.', 'noah-lite' ),
+					'title'       => __( 'Flexible Color Scheme', 'noah-lite' ),
+					'description' => __( 'Match your unique style in an easy and smart way by using an intuitive interface that you can fine-tune it until it fully represents you.', 'noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Background slider/video', 'noah-lite' ),
-					'description' => __( 'Apart from static images, you can use videos or sliders on the background.', 'noah-lite' ),
+					'title'       => __( 'Modular Layout', 'noah-lite' ),
+					'description' => __( 'You have the freedom to play with various design elements in order to adapt the whole digital puzzle in a way you resonate.', 'noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Support', 'noah-lite' ),
-					'description' => __( 'You will benefit of our full support for any issues you have with the theme.', 'noah-lite' ),
+					'title'       => __( 'Advance Typography Settings', 'noah-lite' ),
+					'description' => __( 'Adjust your fonts by taking advantage of a playground with 600+ fonts varieties you can wisely chose from at any moment.', 'noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Packages/Subscribe sections', 'noah-lite' ),
-					'description' => __( 'Add pricing tables for your products and use newsletter forms to attract the clients.', 'noah-lite' ),
+					'title'       => __( 'Social Sharing Features', 'noah-lite' ),
+					'description' => __( 'Start gathering the right people who find your work purposeful and ready to be shared with the world.', 'noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				),
 				array(
-					'title'       => __( 'Change labels for the Contact Us section', 'noah-lite' ),
-					'description' => __( 'Write an original text in each Contact us section field.', 'noah-lite' ),
+					'title'       => __( 'Premium Support', 'noah-lite' ),
+					'description' => __( 'You will benefit by priority support from a caring and devoted team, eager to help and to spread happiness. We work hard to provide a flawless experience for those who vote us with trust and choose to be our special clients.', 'noah-lite' ),
+					'is_in_lite'  => 'false',
+					'is_in_pro'   => 'true',
+				),
+				array(
+					'title'       => __( 'Comprehensive Help Guide', 'noah-lite' ),
+					'description' => __( 'Extensive documentation that will help you get your site up quickly and seamlessly.', 'noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				),
 				array(
 					'title'       => __( 'No credit footer link', 'noah-lite' ),
-					'description' => __( 'Remove "Noah Lite developed by ThemeIsle" copyright from the footer.', 'noah-lite' ),
+					'description' => __( 'Remove "Theme: Noah Lite by Pixelgrade" copyright from the footer area.', 'noah-lite' ),
 					'is_in_lite'  => 'false',
 					'is_in_pro'   => 'true',
 				)
@@ -226,22 +190,13 @@ function noahlite_admin_setup() {
 			'deactivate_label' => esc_html__( 'Deactivate', 'noah-lite' ),
 			'content'                   => array(
 				array(
-					'slug' => 'siteorigin-panels'
+					'slug' => 'jetpack'
 				),
 				array(
-					'slug' => 'wp-product-review'
+					'slug' => 'wordpress-seo'
 				),
 				array(
-					'slug' => 'intergeo-maps'
-				),
-				array(
-					'slug' => 'visualizer'
-				),
-				array(
-					'slug' => 'adblock-notify-by-bweb'
-				),
-				array(
-					'slug' => 'nivo-slider-lite'
+					'slug' => 'gridable'
 				)
 			),
 		),
